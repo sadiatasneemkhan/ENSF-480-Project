708a7e76cf99d2858564fc74405417d47715794c
dhyey <dhyeylalseta@outlook.com>
2021-12-05 12:47:00 -0700

    example view class

---
+++ src/main/java/view/PropertyListingView.java
@@ -0,0 +1,13 @@
+package view;
+
+public class PropertyListingView {
+    // put all GUI stuff here for listing all the properties e.g.
+
+    /**
+     * displayProperties() {
+     *     // set up GUI
+     *     listings = propertyController.getProperties();
+     *     // render listings
+     * }
+     */
+}
