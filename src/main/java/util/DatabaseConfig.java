906a59a56023fc483fb2d8945320ff1c6b4e847e
dhyey <dhyeylalseta@outlook.com>
2021-12-07 17:19:04 -0700

    fixed db config

--- src/main/java/util/DatabaseConfig.java
+++ src/main/java/util/DatabaseConfig.java
@@ -2,6 +2,6 @@ package util;
 
 class DatabaseConfig {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/rentings?connectTimeout=0&socketTimeout=0&autoReconnect=true";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "Sanchit2001!";
+    public static final String DB_USER = "ensf480";
+    public static final String DB_PASSWORD = "password";
 }
